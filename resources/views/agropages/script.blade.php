90096780653cd0a68fe7d6c3dff040ec8c5ce296
hamisihussein <hamisihussein999@gmail.com>
2025-12-13 17:26:44 +0300

    Add assets

--- resources/views/agropages/script.blade.php
+++ resources/views/agropages/script.blade.php
@@ -1,5 +1,5 @@
-<script src="assets/js/jquery-1.11.0.min.js"></script>
-<script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/templatemo.js"></script>
-<script src="assets/js/custom.js"></script>
+<script src="{{asset('js/jquery-1.11.0.min.js')}}"></script>
+<script src="{{asset('js/jquery-migrate-1.2.1.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('js/templatemo.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
