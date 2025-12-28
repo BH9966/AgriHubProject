96c725bb7ff449b890c7a39288e74f8f43b7ac6c
hamisihussein <hamisihussein999@gmail.com>
2025-12-28 13:39:13 +0300

    Modify db

--- database/migrations/2025_12_13_075708_create_order.php
+++ database/migrations/2025_12_13_075708_create_order.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('order', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('buyer_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('buyer_id')->constrained('users')->cascadeOnDelete();
             $table->decimal('total_price', 10, 2);
             $table->enum('status', ['pending', 'confirmed', 'completed'])->default('pending');
             $table->timestamps();
